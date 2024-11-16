61ff10b743872ab821fe3db5bc71c95fbfc1d1c6
Hongwei Bai <bhw8412@gmail.com>
2024-11-17 09:53:15 +1100

    add pager impl

--- app/src/main/java/com/mikeapp/democomposenav/ui/PagerScreen.kt
+++ app/src/main/java/com/mikeapp/democomposenav/ui/PagerScreen.kt
@@ -1,16 +1,68 @@
 package com.mikeapp.democomposenav.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import kotlinx.coroutines.launch
 
 @Composable
 fun PagerScreen(navController: NavController, paddingValues: PaddingValues) {
+    val tabs = listOf("p1", "p2", "p3")
+    val pagerState = rememberPagerState(
+        pageCount = { tabs.size }
+    )
+    val scope = rememberCoroutineScope()
 
-    Scaffold(
+    Column {
+        // Top Indicator Row
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier.tabIndicatorOffset(tabPositions[pagerState.currentPage])
+                )
+            }
+        ) {
+            tabs.forEachIndexed { index, title ->
+                Tab(
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        scope.launch { pagerState.animateScrollToPage(index) }
+                    },
+                    text = { Text(title) }
+                )
+            }
+        }
 
-    ) { innerPadding ->
-
+        // Horizontal Pager
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxSize()
+        ) { page ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Page: ${tabs[page]}", style = MaterialTheme.typography.titleLarge)
+            }
+        }
     }
 }
